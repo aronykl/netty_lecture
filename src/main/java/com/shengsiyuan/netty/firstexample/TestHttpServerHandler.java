30817b459f43a3c6893f56d7c9be009695bc627b
zhouwen <954419002@qq.com>
2017-08-10 22:31:25 +0800

    完善http程序

--- src/main/java/com/shengsiyuan/netty/firstexample/TestHttpServerHandler.java
+++ src/main/java/com/shengsiyuan/netty/firstexample/TestHttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  *
  * @Author: zhouwen
@@ -16,13 +18,62 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        System.out.println("msg: " + msg.getClass().getName());
+
+        System.out.println("remote address: " + ctx.channel().remoteAddress());
+
+        Thread.sleep(8000);
+
         if (msg instanceof HttpRequest) {
+
+            HttpRequest httpRequest = (HttpRequest) msg;
+
+            System.out.println("请求方法名： " + httpRequest.method().name());
+
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
 
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
             ctx.writeAndFlush(response);
+            ctx.channel().close();
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inActive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
     }
 }
