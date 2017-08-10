f7d4f9ad80bf03dc05c5a65605a96a82b0f9effe
zhouwen <954419002@qq.com>
2017-08-10 21:34:19 +0800

    git init

---
+++ src/main/java/com/shengsiyuan/netty/firstexample/TestServer.java
@@ -0,0 +1,31 @@
+package com.shengsiyuan.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ *
+ * @Author: zhouwen
+ * @Date: 2017/8/10 21:16
+ */
+public class TestServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/shengsiyuan/netty/firstexample/TestServerInitializer.java
@@ -0,0 +1,22 @@
+package com.shengsiyuan.netty.firstexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ *
+ * @Author: zhouwen
+ * @Date: 2017/8/10 21:20
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
