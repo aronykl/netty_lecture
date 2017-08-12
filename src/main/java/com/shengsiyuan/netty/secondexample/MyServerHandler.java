27aa00666cfa1d6086d079a1783cb6892485d88d
zhouwen <954419002@qq.com>
2017-08-12 13:02:41 +0800

    netty Socket编程支持

---
+++ src/main/java/com/shengsiyuan/netty/secondexample/MyServerHandler.java
@@ -0,0 +1,28 @@
+package com.shengsiyuan.netty.secondexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ *
+ * @Author: zhouwen
+ * @Date: 2017/8/12 12:29
+ */
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+
+        ctx.channel().writeAndFlush("from server: " + UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
