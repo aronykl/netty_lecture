701b52b790d2a3b86bfd466e5b78285550e7434b
zhouwen <954419002@qq.com>
2017-08-13 23:57:15 +0800

    netty对于websocket的支持

---
+++ src/main/java/com/shengsiyuan/netty/fifthexample/WebSocketServer.java
@@ -0,0 +1,36 @@
+package com.shengsiyuan.netty.fifthexample;
+
+import com.shengsiyuan.netty.thirdexample.MyChatServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * netty基于websocket的服务
+ * @Author: zhouwen
+ * @Date: 2017/8/12 17:10
+ */
+public class WebSocketServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
